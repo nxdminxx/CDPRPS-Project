a27f42ebc84b0e0d303951759cc2d60ac1d20f03
Ndzfh Amrh <nadzifahsamsudin@gmail.com>
2024-01-17 14:03:18 +0800

    comment kiesha

--- Smart-Health-Remote-System/SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/ProvideDiagnosis/DiagnosisRequestController.java
+++ Smart-Health-Remote-System/SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/ProvideDiagnosis/DiagnosisRequestController.java
@@ -1,47 +1,43 @@
-package com.SmartHealthRemoteSystem.SHSR.ProvideDiagnosis;
-import com.SmartHealthRemoteSystem.SHSR.Service.DiagnosisService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+// package com.SmartHealthRemoteSystem.SHSR.ProvideDiagnosis;
+// import com.SmartHealthRemoteSystem.SHSR.Service.DiagnosisService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+// import java.util.List;
+// import java.util.concurrent.ExecutionException;
 
-@RestController
-@RequestMapping("/api/diagnosis") // Base path for all operations related to diagnoses
-public class DiagnosisRequestController {
+// @RestController
+// @RequestMapping("/api/diagnosis") // Base path for all operations related to diagnoses
+// public class DiagnosisRequestController {
 
-    private final DiagnosisService diagnosisService;
+//     private final DiagnosisService diagnosisService;
 
-    //@Autowired
-    public DiagnosisRequestController(DiagnosisService diagnosisService) {
-        this.diagnosisService = diagnosisService;
-    }
-// 
-    // Endpoint to add a new diagnosis
-    @PostMapping("/diagnosisRequest")
-    public ResponseEntity<Diagnosis> addDiagnosis(@RequestBody Diagnosis diagnosis) throws ExecutionException, InterruptedException {
-    Diagnosis savedDiagnosis = diagnosisService.saveDiagnosis(diagnosis);
-    return ResponseEntity.ok(savedDiagnosis); // Returns the saved diagnosis
-}
+//     //@Autowired
+//     public DiagnosisRequestController(DiagnosisService diagnosisService) {
+//         this.diagnosisService = diagnosisService;
+//     }
+// // 
+//     // Endpoint to add a new diagnosis
+//     @PostMapping("/diagnosisRequest")
+//     public ResponseEntity<Diagnosis> addDiagnosis(@RequestBody Diagnosis diagnosis) throws ExecutionException, InterruptedException {
+//     Diagnosis savedDiagnosis = diagnosisService.saveDiagnosis(diagnosis);
+//     return ResponseEntity.ok(savedDiagnosis); // Returns the saved diagnosis
+// }
 
 
  
 
-    // Endpoint to retrieve all diagnoses
-    @GetMapping
-    public ResponseEntity<List<Diagnosis>> getAllDiagnoses() throws ExecutionException, InterruptedException {
-        List<Diagnosis> diagnoses = diagnosisService.getAllDiagnoses();
-        return ResponseEntity.ok(diagnoses); // Returns the list of diagnoses
-    }
+//     // Endpoint to retrieve all diagnoses
+//     @GetMapping
+//     public ResponseEntity<List<Diagnosis>> getAllDiagnoses() throws ExecutionException, InterruptedException {
+//         List<Diagnosis> diagnoses = diagnosisService.getAllDiagnoses();
+//         return ResponseEntity.ok(diagnoses); // Returns the list of diagnoses
+//     }
 
-    @GetMapping("/Diagnosis")
-    public String showDiagnosisPage(Model model) {
-        return "Diagnosis"; // Assuming "Diagnosis.html" is in your templates directory
-    }
 
     
     
     
-}
+// }
--- Smart-Health-Remote-System/SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/Service/DiagnosisService.java
+++ Smart-Health-Remote-System/SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/Service/DiagnosisService.java
@@ -1,32 +1,32 @@
-package com.SmartHealthRemoteSystem.SHSR.Service;
+// package com.SmartHealthRemoteSystem.SHSR.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import com.SmartHealthRemoteSystem.SHSR.ProvideDiagnosis.Diagnosis;
-import com.SmartHealthRemoteSystem.SHSR.ProvideDiagnosis.DiagnosisRepository;
+// import com.SmartHealthRemoteSystem.SHSR.ProvideDiagnosis.Diagnosis;
+// import com.SmartHealthRemoteSystem.SHSR.ProvideDiagnosis.DiagnosisRepository;
 
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+// import java.util.List;
+// import java.util.concurrent.ExecutionException;
 
-@Service
-public class DiagnosisService {
+// @Service
+// public class DiagnosisService {
 
-    private final DiagnosisRepository diagnosisRepository;
+//     private final DiagnosisRepository diagnosisRepository;
 
-    @Autowired
-    public DiagnosisService(DiagnosisRepository diagnosisRepository) {
-        this.diagnosisRepository = diagnosisRepository;
-    }
+//     @Autowired
+//     public DiagnosisService(DiagnosisRepository diagnosisRepository) {
+//         this.diagnosisRepository = diagnosisRepository;
+//     }
  
-    // Save diagnosis
-    public Diagnosis saveDiagnosis(Diagnosis diagnosis) throws ExecutionException, InterruptedException {
-        return diagnosisRepository.save(diagnosis);
-    }
+//     // Save diagnosis
+//     public Diagnosis saveDiagnosis(Diagnosis diagnosis) throws ExecutionException, InterruptedException {
+//         return diagnosisRepository.save(diagnosis);
+//     }
 
-    // Get all diagnoses
-    public List<Diagnosis> getAllDiagnoses() throws ExecutionException, InterruptedException {
-        return diagnosisRepository.findAll();
+//     // Get all diagnoses
+//     public List<Diagnosis> getAllDiagnoses() throws ExecutionException, InterruptedException {
+//         return diagnosisRepository.findAll();
 
         
-    }}
+//     }}
