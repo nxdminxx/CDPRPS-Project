e6a74d70f6960c49c35877fc7137c820de76c01b
nurizzatisaidi <nurizzatisaidi2@gmail.com>
2024-01-17 20:55:47 +0800

    AssignPatient, UnassignPatient & ReleasePatient

--- Smart-Health-Remote-System/SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/AssignPatient/AssignPatientController.java
+++ Smart-Health-Remote-System/SHSR/src/main/java/com/SmartHealthRemoteSystem/SHSR/AssignPatient/AssignPatientController.java
@@ -93,14 +93,17 @@ public class AssignPatientController {
             List<Patient> patientList = allPatients.subList(start, end);
 
         //SMTP - Izzati
+
+        
         var to = patient.getEmail();
-        var subject = "You have been ASSIGNED to a new Doctor";
-        var message = "Hello Dear "+patient.getName()+", We are to inform you that you have been ASSIGNED to a Dr. ("+doctor.getName()+"-"+doctor.getUserId()+")\n\n"
-        +"For any further information, do not hesitate to email us back.\nThank you.";
-        var mailStructure = new MailStructure(to,subject,message);
-        mailService.sendAssignedMail(to, subject, message, mailStructure);
-
-
+        if (to != null && !to.isEmpty()) {
+            var subject = "You have been ASSIGNED to a new Doctor";
+            var message = "Hello Dear "+patient.getName()+", We are to inform you that you have been ASSIGNED to a Dr. ("+doctor.getName()+"-"+doctor.getUserId()+")\n\n"
+            +"For any further information, do not hesitate to email us back.\nThank you.";
+            var mailStructure = new MailStructure(to,subject,message);
+            mailService.sendAssignedMail(to, subject, message, mailStructure);
+        }
+        
         model.addAttribute("startIndex", startIndex);
         model.addAttribute("currentPage", pageNo);
         model.addAttribute("totalPages", (total + pageSize - 1) / pageSize);
@@ -111,48 +114,103 @@ public class AssignPatientController {
     }
 
     @PutMapping("/unassignDoctor")
-    public String UnassignDoctor(Model model,@RequestParam (value="patientId")String patientID) throws ExecutionException, InterruptedException {
+    public String UnassignDoctor(Model model,@RequestParam (value="patientId")String patientID,  @RequestParam(defaultValue = "0") int pageNo, 
+    @RequestParam(defaultValue = "5") int pageSize, @RequestParam(defaultValue = "") String searchQuery) throws ExecutionException, InterruptedException {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         MyUserDetails myUserDetails= (MyUserDetails) auth.getPrincipal();
         Doctor doctor = assignPatientServices.getDoctor(myUserDetails.getUsername());
         Patient patient =assignPatientServices.getPatient(patientID);
         assignPatientServices.UnassignDoctor(patientID,doctor.getUserId());
-        List<Patient> patientList=assignPatientServices.getListPatient();
+        // List<Patient> patientList=assignPatientServices.getListPatient();
+
+        List<Patient> allPatients = assignPatientServices.getListPatient();
+
+
+        if (!searchQuery.isEmpty()) {
+            allPatients = allPatients.stream()
+                                     .filter(p -> p.getName().toLowerCase().contains(searchQuery.toLowerCase()) 
+                                               || p.getUserId().toString().contains(searchQuery))
+                                     .collect(Collectors.toList());
+        }
+    
+            int total = allPatients.size();
+            int start = Math.min(pageNo * pageSize, total);
+            int end = Math.min((pageNo + 1) * pageSize, total);
+            int startIndex = pageNo * pageSize;
+
+
+            List<Patient> patientList = allPatients.subList(start, end);
 
         // SMTP - Izzati
         var to = patient.getEmail();
-        var subject = "You have been UNASSIGNED from a Doctor";
-        var message = "Hello Dear "+patient.getName()+", We are to inform you that you have been UNASSIGNED from  Dr. ("+doctor.getName()+"-"+doctor.getUserId()+")\n\n"
-        +"For any further information, do not hesitate to email us back.\nThank you.";
-        var mailStructure = new MailStructure(to,subject,message);
-        mailService.sendUnassignedMail(to, subject, message, mailStructure);
+        if (to != null && !to.isEmpty()) {
+            var subject = "You have been UNASSIGNED from a Doctor";
+            var message = "Hello Dear " + patient.getName() + ", We are to inform you that you have been UNASSIGNED from Dr. (" + doctor.getName() + "-" + doctor.getUserId() + ")\n\n"
+                    + "For any further information, do not hesitate to email us back.\nThank you.";
+            var mailStructure = new MailStructure(to, subject, message);
+            mailService.sendUnassignedMail(to, subject, message, mailStructure);
+        } 
 
+
+        
+        model.addAttribute("startIndex", startIndex);
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", (total + pageSize - 1) / pageSize);
         model.addAttribute("patientList",patientList);
         model.addAttribute("doctor",doctor);
+        model.addAttribute("searchQuery", searchQuery);
+
         return "assignpatient";
 
     }
     
     @PostMapping("/releasepatient")
-    public String ReleasePatient(Model model,@RequestParam (value ="patientId")String patientID) throws ExecutionException, InterruptedException {
+    public String ReleasePatient(Model model,@RequestParam (value ="patientId")String patientID, @RequestParam(defaultValue = "0") int pageNo, 
+    @RequestParam(defaultValue = "5") int pageSize, @RequestParam(defaultValue = "") String searchQuery) throws ExecutionException, InterruptedException {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         MyUserDetails myUserDetails= (MyUserDetails) auth.getPrincipal();
         Doctor doctor = assignPatientServices.getDoctor(myUserDetails.getUsername());
         Patient patient =assignPatientServices.getPatient(patientID);
         assignPatientServices.ReleasePatient(patientID,doctor.getUserId());
-        List<Patient> patientList=assignPatientServices.getListPatient();
+        // List<Patient> patientList=assignPatientServices.getListPatient();
+
+        List<Patient> allPatients = assignPatientServices.getListPatient();
+
+
+        if (!searchQuery.isEmpty()) {
+            allPatients = allPatients.stream()
+                                     .filter(p -> p.getName().toLowerCase().contains(searchQuery.toLowerCase()) 
+                                               || p.getUserId().toString().contains(searchQuery))
+                                     .collect(Collectors.toList());
+        }
+    
+            int total = allPatients.size();
+            int start = Math.min(pageNo * pageSize, total);
+            int end = Math.min((pageNo + 1) * pageSize, total);
+            int startIndex = pageNo * pageSize;
+
+
+            List<Patient> patientList = allPatients.subList(start, end);
 
         //SMTP - Izzati
         var to = patient.getEmail();
+
+        if (to != null && !to.isEmpty()) {
         var subject = "You have been RELEASED from a Doctor";
         var message = "Hello Dear "+patient.getName()+", We are to inform you that you have been RELEASED from  Dr. ("+doctor.getName()+"-"+doctor.getUserId()+")\n\n"
         +"For any further information, do not hesitate to email us back.\nThank you.";
         var mailStructure = new MailStructure(to,subject,message);
         mailService.sendUnassignedMail(to, subject, message, mailStructure);
+        }
 
+        model.addAttribute("startIndex", startIndex);
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", (total + pageSize - 1) / pageSize);
         model.addAttribute("patientList",patientList);
         model.addAttribute("doctor",doctor);
+        model.addAttribute("searchQuery", searchQuery);
+        
         return "assignpatient";
     }
 }
